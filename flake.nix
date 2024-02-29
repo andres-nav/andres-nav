98b3a10dd9d83913131a097d9fa20ba474eb80e9
Andres Navarro <contact@andresnav.com>
2024-02-29 14:23:17 +0100

    add: flake.nix file

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "personal website (andresnav.com)";
+
+  outputs = { self, nixpkgs, }:
+    let system = "x86_64-linux";
+    in {
+      devShells.${system}.default = with import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+        mkShell { packages = [ gnumake ]; };
+    };
+}
